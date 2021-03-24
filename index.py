64c7c646ed733b41175e96748a5afd3e3dc30cc6
Steven Attwell <stevenattwell@hotmail.com>
2021-03-24 18:05:57 +0000

    initial commit

--- index.py
+++ index.py
@@ -3,10 +3,13 @@ import datetime
 
 
 def handler(event, context):
+    id = event['id']
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'id': id
     }
+    print("Lambda", context.invoked_function_arn)
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
